0fbcc01a2f972d5138005a35145701a494d6678a
unknown <20CS021@charusat.edu.in>
2023-09-08 01:11:18 +0530

    Made ngrok_.py compatible for multiple server start

--- ngrok_.py
+++ ngrok_.py
@@ -7,8 +7,10 @@ import multiprocessing
 import re 
 import time
 import asyncio
-
+import inspect
 import ssl
+import os
+
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -27,7 +29,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # reduced_string = re.sub(r'.', '',ssh_tunnel.public_url , count = 6)
 # print(reduced_string)
 
-PORT_NUMBER = 8051
+
 
 
 def sleep():
@@ -45,6 +47,18 @@ def main_init(region_name:str = 'in', port_number: int = 8051):
     print("Copy and paste this URL in your browser: ",reduced_string)
 
 def async_tasks():
+    
+    # Getting the information of which file the function was called form
+    calling_file = inspect.currentframe().f_back.f_code.co_filename
+    
+    # Used to trim so that we can get actual file name
+    length = len(str(os.getcwd())) + 1
+    if calling_file[length:] == 'ngrok_.py' or 'frontend.py':
+        PORT_NUMBER = 8051
+    else:
+        PORT_NUMBER = 10000
+    
+    print("Calling function name =", calling_file[length:], 'port number =', PORT_NUMBER)
     region_name = input("Enter the region: ")
     main_init(region_name=region_name, port_number=PORT_NUMBER)
     print("Before Sleep")
